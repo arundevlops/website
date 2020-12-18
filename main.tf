5de4faf9384351092f1fc53e2976f2794c83fb38
arundevlops <btech.arung@gmail.com>
2020-12-19 02:55:07 +0530

    'a'

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "AKIA5DWCIY25DJYFX4FV",
-    secret_key = "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",
+    access_key = "AKIA5DWCIY25DJYFX4FV"
+    secret_key = "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"
     region = "${var.aws_region}"
 }
 
